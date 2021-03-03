d0584782e0ec227d4bad0456033d8145e972dafc
Muhammad Rezky <mhdrezky@gmail.com>
2021-03-03 10:12:32 +0700

    Reset

--- template/ios/Pods/Target Support Files/Pods-ReactNativeChky-ReactNativeChkyTests/Pods-ReactNativeChky-ReactNativeChkyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeChky_ReactNativeChkyTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeChky_ReactNativeChkyTests
-@end
--- template/ios/Pods/Target Support Files/Pods-ReactNativeChky-tvOSTests/Pods-ReactNativeChky-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeChky_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeChky_tvOSTests
-@end
