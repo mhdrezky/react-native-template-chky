b33aef1d2a3d2c68b1eb251317324e0b304ac014
Muhammad Rezky <mhdrezky@gmail.com>
2021-03-03 09:41:03 +0700

    React Native Template

---
+++ template/ios/Pods/Target Support Files/Pods-ReactNativeChky-ReactNativeChkyTests/Pods-ReactNativeChky-ReactNativeChkyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeChky_ReactNativeChkyTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeChky_ReactNativeChkyTests
+@end
